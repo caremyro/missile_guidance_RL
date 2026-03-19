9e5f34583bf67d57e49df5d6cdb8c659b511767c
remy <remy.gouhier.pro@gmail.com>
2026-03-19 20:38:44 +0100

    FEAT: aircraft following straight line trajectory

--- missile_chase_target_RL.py
+++ missile_chase_target_RL.py
@@ -29,13 +29,78 @@ class MissileEnv(gym.Env):
         ])
         return obs.astype(np.float32)
 
+    def _update_aircraft(self):
+        dx0 = np.cos(self.cap_initial)
+        dy0 = np.sin(self.cap_initial)
+
+        if 0 <= self.t <= self.Straight_time:
+            x = self.aircraft_start_loc[0] + self.targ_vel * dx0 * self.t
+            y = self.aircraft_start_loc[1] + self.targ_vel * dy0 * self.t
+            z = self.aircraft_start_loc[2]
+
+        elif self.Straight_time < self.t <= self.Straight_time + self.curve_time:
+            tc = self.t - self.Straight_time
+            if not self.curve_initialized:
+                self.curve_start_x = self.aircraft_pos[0]
+                self.curve_start_y = self.aircraft_pos[1]
+                self.curve_start_z = self.aircraft_pos[2]
+                self.curve_initialized = True
+                sign = np.sign(self.turn_angle)
+                self.center_x = self.curve_start_x + abs(self.radius) * (-sign * dy0)
+                self.center_y = self.curve_start_y + abs(self.radius) * ( sign * dx0)
+                self.center_z = self.curve_start_z
+                self.curve_initialized = True
+            frac      = tc / self.curve_time
+            angle_now = frac * self.turn_angle
+            vx0 =  np.sign(self.turn_angle) * dy0
+            vy0 = -np.sign(self.turn_angle) * dx0
+            cos_a, sin_a = np.cos(angle_now), np.sin(angle_now)
+            vx = vx0 * cos_a - vy0 * sin_a
+            vy = vx0 * sin_a + vy0 * cos_a
+
+            x = self.center_x + abs(self.radius) * vx
+            y = self.center_y + abs(self.radius) * vy
+            z = self.curve_start_z \
+                + np.sin(self.yz_angle + np.pi/2) * self.targ_vel**2 \
+                * (1 - np.cos(np.pi * tc / self.curve_time)) * self.climb_rate_curve
+
+        elif self.Straight_time + self.curve_time < self.t <= self.Straight_time + self.curve_time + self.Straight_time2:
+            if not self.straight2_initialized:
+                self.straight2_start_x = self.aircraft_pos[0]
+                self.straight2_start_y = self.aircraft_pos[1]
+                self.straight2_start_z = self.aircraft_pos[2]
+                self.straight2_initialized = True
+
+            ts        = self.t - (self.Straight_time + self.curve_time)
+            cap_final = self.cap_initial + self.turn_angle
+            x = self.straight2_start_x + self.targ_vel * ts * np.cos(cap_final)
+            y = self.straight2_start_y + self.targ_vel * ts * np.sin(cap_final)
+            z = self.straight2_start_z
+
+        else:
+            if self.straight2_initialized:
+                cap_final = self.cap_initial + self.turn_angle
+                x = self.straight2_start_x + self.targ_vel * self.Straight_time2 * np.cos(cap_final)
+                y = self.straight2_start_y + self.targ_vel * self.Straight_time2 * np.sin(cap_final)
+                z = self.straight2_start_z
+            else:
+                x = self.aircraft_start_loc[0] + self.targ_vel * dx0 * self.Straight_time
+                y = self.aircraft_start_loc[1] + self.targ_vel * dy0 * self.Straight_time
+                z = self.aircraft_start_loc[2]
+            
+        if self.t > 0:
+            self.aircraft_vel = (np.array([x, y, z]) - self.aircraft_pos) / self.dt
+
+
+        return np.array([x, y, z])
+
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
         rng = np.random.default_rng(seed)
 
         altitude_avion = rng.uniform(8000, 15000)
         cap_initial = rng.uniform(0, 2 * np.pi)
-
+        self.cap_initial = cap_initial
         self.aircraft_start_loc = np.array([
             rng.uniform(-20000, 20000),
             rng.uniform(-20000, 20000),
@@ -70,6 +135,13 @@ class MissileEnv(gym.Env):
         self.prev_distance = np.linalg.norm(self.aircraft_pos - self.missile_pos)
         self.t = 0.0
 
+        self.curve_initialized    = False
+        self.straight2_initialized = False
+        self.curve_start          = None
+        self.straight2_start      = None
+        self.center               = None
+        self.radius               = (self.targ_vel * self.curve_time) / self.turn_angle
+
         return self._get_obs(), {}
 
     def step(self, action):
@@ -83,7 +155,7 @@ class MissileEnv(gym.Env):
             self.missile_pos[2] = 0
             self.missile_vel[2] = 0
 
-        self.aircraft_pos += self.aircraft_vel * self.dt
+        self.aircraft_pos = self._update_aircraft()
         self.t += self.dt
 
         done = False 
